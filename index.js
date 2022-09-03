54af6bf8dbb8fa712b64a1efab3c63d2cdfbba98
Atulya subedi <109258174+atulsubedi@users.noreply.github.com>
2022-09-03 12:50:34 +0000

    fix:docs

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 var readlinesync = require("readline-sync");
 var userName = readlinesync.question("What is your name? ");
+
+
 console.log("Welcome " + userName + " to this quiz let's see how well do you know me! ");
 console.log("-----------------")
 
+var score = 0;
 
 
 
 
 
-var readlinesync = require("readline-sync");
 var userQuestion = readlinesync.question("Where do I live? ");
 var userAnswer = ("nepal");
-var score = 0;
 if (userQuestion === userAnswer) {
   console.log("you are right");
+  score = score + 1;
 
 
 } else {
   console.log("you are wrong");
-
+  score = score + 0;
 }
 
 
@@ -28,14 +30,14 @@ console.log("-----------------")
 
 var userTwo = readlinesync.question("How old am I? ");
 var userAnswerTwo = ("15");
-var score = 0;
+
 if (userTwo === userAnswerTwo) {
   console.log("you are right");
-
+  score = score + 1;
 
 } else {
   console.log("you are wrong");
-
+  score = score + 0;
 }
 
 
@@ -44,14 +46,14 @@ console.log("-----------------");
 
 var userQuestionThree = readlinesync.question("What is my favorite food? ");
 var userAnswerThree = ("goat meat");
-var score = 0;
+
 if (userQuestionThree === userAnswerThree) {
   console.log("you are right");
-
+  score = score + 1;
 
 } else {
   console.log("you are wrong");
-
+  score = score + 0;
 }
 
 
@@ -63,14 +65,14 @@ console.log("-----------------")
 
 var userQuestionFour = readlinesync.question("What is my favorite place? ");
 var userAnswerFour = ("jomson");
-var score = 0;
+
 if (userQuestionFour === userAnswerFour) {
   console.log("you are right");
-
+  score = score + 1;
 
 } else {
   console.log("you are wrong");
-
+  score = score + 0;
 }
 
 
@@ -82,16 +84,104 @@ console.log("-----------------")
 
 var userQuestionFive = readlinesync.question("What is my favorite coffe? ");
 var userAnswerFive = ("ice coffe");
-var score = 0;
+
 if (userQuestionFive === userAnswerFive) {
   console.log("you are right");
-
+  score = score + 1;
 
 } else {
   console.log("you are wrong");
-
+  score = score + 0;
 }
 
 
 console.log("-----------------")
+
+
+
+var userQuestionSix = readlinesync.question("Which series i like the most? ");
+var userAnswerSix = ("never have i ever");
+
+if (userQuestionSix === userAnswerSix) {
+  console.log("you are right");
+  score = score + 1;
+
+} else {
+  console.log("you are wrong");
+  score = score + 0;
+}
+
+
+console.log("-----------------")
+
+
+
+var userQuestionSeven = readlinesync.question("Which fruit i like the most? ");
+var userAnswerSeven = ("apple");
+
+if (userQuestionSeven === userAnswerSeven) {
+  console.log("you are right");
+  score = score + 1;
+
+} else {
+  console.log("you are wrong");
+  score = score + 0;
+}
+
+
+console.log("-----------------")
+
+
+
+
+var userQuestionEight = readlinesync.question("Is rainny day my favourite day? ");
+var userAnswerEight = ("yes");
+
+if (userQuestionEight === userAnswerEight) {
+  console.log("you are right");
+  score = score + 1;
+
+} else {
+  console.log("you are wrong");
+  score = score + 0;
+}
+
+
+console.log("-----------------")
+
+var userQuestionNine = readlinesync.question("Which is my favourite season? ");
+var userAnswerNine = ("winter");
+
+if (userQuestionNine === userAnswerNine) {
+  console.log("you are right");
+  score = score + 1;
+
+} else {
+  console.log("you are wrong");
+  score = score + 0;
+}
+
+
+console.log("-----------------")
+
+
+
+
+
+
+var userQuestionTen = readlinesync.question("Which is my favourite team in fifa world cup? ");
+var userAnswerTen = ("france");
+
+if (userQuestionTen === userAnswerTen) {
+  console.log("you are right");
+
+  score = score + 1;
+} else {
+  console.log("you are wrong");
+  score = score + 0;
+}
+
+
+console.log("-----------------")
+console.log("Your score is:" + score);
 console.log("---Quiz Ended---")
